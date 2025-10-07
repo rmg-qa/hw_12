6a93ae0e859a27777efacfddee94e8c36a6ff38a
rmg-qa <gorohovikroman123@yandex.com>
2025-10-07 22:35:04 +0700

    add allure.step

--- tests/test_part_2_send_form_registration.py
+++ tests/test_part_2_send_form_registration.py
@@ -4,13 +4,16 @@ from pages.page_registration_form import RegistrationFormMethods, SimpleRegistra
 from data.user import user_student
 
 
-@allure.title("Successful fill form")
-def test_send_form_part_2(setup_browser):
+@allure.title("Successful fill form registration")
+def test_send_form_registration(setup_browser):
     user = user_student
-    open_page_registrations = RegistrationFormMethods()
-    simple_registrations = SimpleRegistration()
-    open_page_registrations.open_page(setup_browser)  # открытие страницы регистрации
-    browser.driver.execute_script("$('#fixedban').remove()")
-    browser.driver.execute_script("$('footer').remove()")
-    simple_registrations.simple_registration(setup_browser, user)  # регистрация нового пользователя
-    simple_registrations.assert_simple_registration(setup_browser)  # проверка, что регистрация прошла успешно
+    with allure.step('Открываем страницу формы'):
+        open_page_registrations = RegistrationFormMethods()
+        simple_registrations = SimpleRegistration()
+        open_page_registrations.open_page(setup_browser)
+        browser.driver.execute_script("$('#fixedban').remove()")
+        browser.driver.execute_script("$('footer').remove()")
+    with allure.step('Процесс регистрации нового пользователя'):
+        simple_registrations.simple_registration(setup_browser, user)
+    with allure.step('Проверка, что регистрация прошла успешно'):
+        simple_registrations.assert_simple_registration(setup_browser)
